298ba70ecd41a28b8a358059ec8f2e5f83ee16b9
cataand <52856673+cataand@users.noreply.github.com>
2023-01-19 15:39:42 +0100

    Check if Make running from virtual environment

--- test_environment.py
+++ test_environment.py
@@ -2,7 +2,23 @@ import sys
 
 REQUIRED_PYTHON = (3, 9)
 
-if __name__ == "__main__":
+
+def check_python_virtual_environment():
+    base_prefix = (
+        getattr(sys, "base_prefix", None)
+        or getattr(sys, "real_prefix", None)
+        or sys.prefix
+    )
+    current_prefix = sys.prefix
+
+    if base_prefix == current_prefix:
+        raise TypeError(
+            "It looks like you are not in a virtual environment, "
+            "this project works best if you are in one, so please switch to one."
+        )
+
+
+def check_python_version():
     found_python = (sys.version_info.major, sys.version_info.minor)
 
     if REQUIRED_PYTHON != found_python:
@@ -12,3 +28,8 @@ if __name__ == "__main__":
                 ".".join(map(str, found_python)),
             )
         )
+
+
+if __name__ == "__main__":
+    check_python_virtual_environment()
+    check_python_version()
