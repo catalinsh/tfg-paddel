1e19b8c49888556ef4b59d7befbe1559d8ccf91d
cataand <52856673+cataand@users.noreply.github.com>
2023-04-02 15:34:34 +0200

    Basic navbar

--- app/web/pages/_document.tsx
+++ app/web/pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className=" bg-white text-stone-900 dark:bg-stone-900 dark:text-white">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
