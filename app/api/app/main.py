8c96056579230347a93a2fcccc80c7159a7f33bc
cataand <52856673+cataand@users.noreply.github.com>
2023-04-08 23:06:27 +0200

    Add basic user endpoints

--- app/api/app/main.py
+++ app/api/app/main.py
@@ -1,7 +1,5 @@
-from typing import Union
-from urllib.parse import urlparse
-
-from fastapi import FastAPI
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends, HTTPException
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
@@ -9,19 +7,38 @@ from .database import SessionLocal, engine
 models.Base.metadata.create_all(bind=engine)
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Paddel",
+    description="Documentation for the API for the PaDDeL website.",
+    version="0.1",
+)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "asdf"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/users/", response_model=schemas.User, tags=["users"])
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    return crud.create_user(db=db, user=user)
 
 
-@app.get("/times_two/{number}")
-def times_two(number: int):
-    return {"result": number * 2}
+@app.get("/users/", response_model=list[schemas.User], tags=["users"])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db=db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User, tags=["users"])
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
