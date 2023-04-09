1c24b54afb7c54401812f934817a98cd86a6502c
cataand <52856673+cataand@users.noreply.github.com>
2023-04-09 23:07:49 +0200

    First iteration of video upload endpoint

--- app/api/app/main.py
+++ app/api/app/main.py
@@ -1,8 +1,13 @@
+from tempfile import NamedTemporaryFile
+
 from sqlalchemy.orm import Session
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, UploadFile
 
 from . import crud, models, schemas
 from .database import SessionLocal, engine
+from paddel.enums import Side, Gender
+from paddel.preprocessing.input.features import extract_video_features
+from paddel import settings
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -42,3 +47,31 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@app.post("/predict/")
+def obtain_prediction(
+    hand_in_video: Side,
+    dominant_hand: Side,
+    gender: Gender,
+    video: UploadFile,
+):
+    file = NamedTemporaryFile()
+    file_path = file.name
+
+    contents = video.file.read()
+    with open(file_path, "wb") as f:
+        f.write(contents)
+
+    (
+        classic_features,
+        fresh_features,
+        detection_time,
+    ) = extract_video_features(file_path)
+
+    if detection_time < settings.min_detection_seconds:
+        raise HTTPException(
+            status_code=422, detail="Not enough hand poses detected in provided video."
+        )
+
+    return classic_features.to_dict()
