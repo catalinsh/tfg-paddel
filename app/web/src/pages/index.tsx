500203577fd5e4123159f340c72151ed01d4c898
cataand <52856673+cataand@users.noreply.github.com>
2023-04-10 22:39:33 +0200

    Add predict form scaffold

--- app/web/src/pages/index.tsx
+++ app/web/src/pages/index.tsx
@@ -1,5 +1,10 @@
 import Link from "next/link";
-import { UserIcon, ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import {
+  UserIcon,
+  ArrowTopRightOnSquareIcon,
+  VideoCameraIcon,
+  UserCircleIcon,
+} from "@heroicons/react/20/solid";
 
 import Logo from "@/components/Logo";
 import ButtonSecondary from "@/components/ButtonSecondary";
@@ -46,6 +51,186 @@ const Home = () => (
           </Link>
         </div>
       </nav>
+
+      <form>
+        <div className="mt-12">
+          <div className="border-b border-stone-900/10 pb-12 dark:border-white/10">
+            <h2 className="text-xl font-semibold leading-7 text-stone-900 dark:text-white">
+              Predecir
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-stone-600 dark:text-stone-400">
+              Introduzca la siguiente información para obtener una predicción.
+            </p>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <div className="text-base font-semibold leading-6 text-stone-900 dark:text-white">
+                  Su mano dominante
+                </div>
+                <div className="mt-2 space-y-3">
+                  <div className="flex items-center gap-x-3">
+                    <input
+                      id="dominant-hand-left"
+                      name="dominant-hand"
+                      type="radio"
+                      value={0}
+                      required
+                      className="h-4 w-4 border-stone-300 text-indigo-600 focus:ring-indigo-600 dark:border-stone-600 dark:bg-stone-700 dark:text-indigo-400 dark:ring-offset-stone-900 dark:focus:ring-indigo-400"
+                    />
+                    <label
+                      htmlFor="dominant-hand-left"
+                      className="block text-sm font-medium leading-6 text-stone-900 dark:text-white"
+                    >
+                      Izquierda
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-x-3">
+                    <input
+                      id="dominant-hand-right"
+                      name="dominant-hand"
+                      type="radio"
+                      value={1}
+                      required
+                      className="h-4 w-4 border-stone-300 text-indigo-600 focus:ring-indigo-600 dark:border-stone-600 dark:bg-stone-700 dark:text-indigo-400 dark:ring-offset-stone-900 dark:focus:ring-indigo-400"
+                    />
+                    <label
+                      htmlFor="dominant-hand-right"
+                      className="block text-sm font-medium leading-6 text-stone-900 dark:text-white"
+                    >
+                      Derecha
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <div className="text-base font-semibold leading-6 text-stone-900 dark:text-white">
+                  Mano que muestra en el vídeo
+                </div>
+                <div className="mt-2 space-y-3">
+                  <div className="flex items-center gap-x-3">
+                    <input
+                      id="video-hand-left"
+                      name="video-hand"
+                      type="radio"
+                      value={0}
+                      required
+                      className="h-4 w-4 border-stone-300 text-indigo-600 focus:ring-indigo-600 dark:border-stone-600 dark:bg-stone-700 dark:text-indigo-400 dark:ring-offset-stone-900 dark:focus:ring-indigo-400"
+                    />
+                    <label
+                      htmlFor="video-hand-left"
+                      className="block text-sm font-medium leading-6 text-stone-900 dark:text-white"
+                    >
+                      Izquierda
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-x-3">
+                    <input
+                      id="video-hand-right"
+                      name="video-hand"
+                      type="radio"
+                      value={1}
+                      required
+                      className="h-4 w-4 border-stone-300 text-indigo-600 focus:ring-indigo-600 dark:border-stone-600 dark:bg-stone-700 dark:text-indigo-400 dark:ring-offset-stone-900 dark:focus:ring-indigo-400"
+                    />
+                    <label
+                      htmlFor="video-hand-right"
+                      className="block text-sm font-medium leading-6 text-stone-900 dark:text-white"
+                    >
+                      Derecha
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              <div className="sm:col-span-6">
+                <div className="text-base font-semibold leading-6 text-stone-900 dark:text-white">
+                  Sexo
+                </div>
+                <div className="mt-2 space-y-3">
+                  <div className="flex items-center gap-x-3">
+                    <input
+                      id="sex-male"
+                      name="sex"
+                      type="radio"
+                      value={0}
+                      required
+                      className="h-4 w-4 border-stone-300 text-indigo-600 focus:ring-indigo-600 dark:border-stone-600 dark:bg-stone-700 dark:text-indigo-400 dark:ring-offset-stone-900 dark:focus:ring-indigo-400"
+                    />
+                    <label
+                      htmlFor="sex-male"
+                      className="block text-sm font-medium leading-6 text-stone-900 dark:text-white"
+                    >
+                      Masculino
+                    </label>
+                  </div>
+                  <div className="flex items-center gap-x-3">
+                    <input
+                      id="sex-female"
+                      name="sex"
+                      type="radio"
+                      value={1}
+                      required
+                      className="h-4 w-4 border-stone-300 text-indigo-600 focus:ring-indigo-600 dark:border-stone-600 dark:bg-stone-700 dark:text-indigo-400 dark:ring-offset-stone-900 dark:focus:ring-indigo-400"
+                    />
+                    <label
+                      htmlFor="sex-female"
+                      className="block text-sm font-medium leading-6 text-stone-900 dark:text-white"
+                    >
+                      Femenino
+                    </label>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-span-full">
+                <label
+                  htmlFor="cover-photo"
+                  className="block text-base font-semibold leading-6 text-stone-900 dark:text-white"
+                >
+                  Vídeo
+                </label>
+                <div className="mt-2 flex justify-center rounded-lg border border-dashed border-stone-900/25 px-6 py-10 dark:border-stone-100/25">
+                  <div className="text-center">
+                    <VideoCameraIcon
+                      className="mx-auto h-12 w-12 text-gray-300"
+                      aria-hidden="true"
+                    />
+                    <div className="mt-4 flex text-sm leading-6 text-stone-600 dark:text-stone-400">
+                      <label
+                        htmlFor="hand-video-upload"
+                        className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500 dark:bg-stone-900 dark:text-indigo-400 dark:focus-within:ring-indigo-400"
+                      >
+                        <span>Suba un archivo</span>
+                        <input
+                          id="hand-video-upload"
+                          name="hand-video-upload"
+                          type="file"
+                          className="sr-only"
+                          required
+                        />
+                      </label>
+                      <p className="pl-1">o arrastre y suelte</p>
+                    </div>
+                    <p className="text-xs leading-5 text-stone-600 dark:text-stone-400">
+                      MP4, MOV, AVI hasta 100MB
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400"
+          >
+            Obtener predicción
+          </button>
+        </div>
+      </form>
 
       <footer>
         <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
