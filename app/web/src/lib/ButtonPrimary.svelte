68b870c66fe0c7be8b3bb2fb7694f24a26296358
cataand <52856673+cataand@users.noreply.github.com>
2023-05-15 18:56:46 +0200

    Fix #14

--- app/web/src/lib/ButtonPrimary.svelte
+++ app/web/src/lib/ButtonPrimary.svelte
@@ -6,6 +6,7 @@
 
 {#if href}
 	<a
+		data-sveltekit-preload-data="tap"
 		{href}
 		{...$$restProps}
 		class="{clazz} flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
