854c8b155e14c812a064bb5736f35eb0babf05ea
cataand <52856673+cataand@users.noreply.github.com>
2023-06-03 17:13:01 +0200

    fix subcommand exit

--- train.py
+++ train.py
@@ -1,7 +1,6 @@
-import io
-import os
 import pickle
-import sys
+from contextlib import contextmanager, redirect_stderr, redirect_stdout
+from os import devnull
 from pathlib import Path
 
 import click
@@ -20,17 +19,30 @@ from sklearn.preprocessing import QuantileTransformer
 from paddel.preprocessing import get_data
 from paddel.preprocessing.transformer import FeatureSelector
 
+clf = None
+
+
+@contextmanager
+def suppress_stdout_stderr():
+    """A context manager that redirects stdout and stderr to devnull"""
+    with open(devnull, 'w') as fnull:
+        with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
+            yield (err, out)
+
 
 class ChoiceOption(click.Option):
     def __init__(self, param_decls=None, **attrs):
         click.Option.__init__(self, param_decls, **attrs)
         if not isinstance(self.type, click.Choice):
-            raise Exception('ChoiceOption type arg must be click.Choice')
+            raise Exception("ChoiceOption type arg must be click.Choice")
 
         if self.prompt:
-            prompt_text = '{}:\n{}\n'.format(
+            prompt_text = "{}:\n{}\n".format(
                 self.prompt,
-                '\n'.join(f'{idx: >4}: {c}' for idx, c in enumerate(self.type.choices, start=1))
+                "\n".join(
+                    f"{idx: >4}: {c}"
+                    for idx, c in enumerate(self.type.choices, start=1)
+                ),
             )
             self.prompt = prompt_text
 
@@ -45,46 +57,51 @@ class ChoiceOption(click.Option):
 
         prompt_type = click.IntRange(min=1, max=len(self.type.choices))
         return click.prompt(
-            self.prompt, default=default, type=prompt_type,
-            hide_input=self.hide_input, show_choices=False,
+            self.prompt,
+            default=default,
+            type=prompt_type,
+            hide_input=self.hide_input,
+            show_choices=False,
             confirmation_prompt=self.confirmation_prompt,
-            value_proc=lambda x: self.process_prompt_value(ctx, x, prompt_type))
+            value_proc=lambda x: self.process_prompt_value(ctx, x, prompt_type),
+        )
 
 
 @click.command()
 @click.option(
-    '--c',
-    prompt='Regularization parameter',
+    "--c",
+    prompt="Regularization parameter",
     default=1.0,
-    type=click.FloatRange(min=0, min_open=True)
+    type=click.FloatRange(min=0, min_open=True),
 )
 @click.option(
-    '--kernel',
-    prompt='SVM kernel to use',
+    "--kernel",
+    prompt="SVM kernel to use",
     default=3,
-    type=click.Choice(['linear', 'poly', 'rbf', 'sigmoid', 'precomputed']),
-    cls=ChoiceOption
+    type=click.Choice(["linear", "poly", "rbf", "sigmoid", "precomputed"]),
+    cls=ChoiceOption,
 )
 @click.option(
-    '--degree',
-    prompt='Degree of the function (only for poly kernel)',
+    "--degree",
+    prompt="Degree of the function (only for poly kernel)",
     default=2,
-    type=click.INT
+    type=click.INT,
 )
 @click.option(
-    '--coef0',
-    prompt='Independent term for kernel function (only for poly and sigmoid kernel)',
+    "--coef0",
+    prompt="Independent term for kernel function (only for poly and sigmoid kernel)",
     default=0.0,
-    type=click.INT
+    type=click.INT,
 )
 @click.option(
-    '--tol',
-    prompt='Stopping criterion tolerance',
+    "--tol",
+    prompt="Stopping criterion tolerance",
     default=1e-3,
-    type=click.FloatRange(min=0, min_open=True)
+    type=click.FloatRange(min=0, min_open=True),
 )
 def svc_function(c, kernel, degree, coef0, tol):
-    return SVC(
+    global clf
+    clf = SVC(
         C=c,
         kernel=kernel,
         degree=degree,
@@ -95,27 +112,28 @@ def svc_function(c, kernel, degree, coef0, tol):
 
 
 def nb_function():
-    return GaussianNB()
+    global clf
+    clf = GaussianNB()
 
 
 @click.command()
 @click.option(
-    '--n_neighbors',
-    prompt='Number of neighbors',
-    default=1,
-    type=click.IntRange(min=1)
+    "--n_neighbors", prompt="Number of neighbors", default=5, type=click.IntRange(min=1)
 )
 @click.option(
-    '--metric',
-    prompt='Distance metric',
+    "--metric",
+    prompt="Distance metric",
     default=1,
-    type=click.Choice(["euclidean", "manhattan", "haversine", "cosine", "nan_euclidean"]),
-    cls=ChoiceOption
+    type=click.Choice(
+        ["euclidean", "manhattan", "haversine", "cosine", "nan_euclidean"]
+    ),
+    cls=ChoiceOption,
 )
-def knn_function(n_neighbors):
-    return KNeighborsClassifier(
+def knn_function(n_neighbors, metric):
+    global clf
+    clf = KNeighborsClassifier(
         n_neighbors=n_neighbors,
-
+        metric=metric,
     )
 
 
@@ -133,59 +151,63 @@ algorithmFunctions = {
 
 @click.command()
 @click.option(
-    '--video_dir',
-    prompt='Directory where videos are located',
-    default='./data/raw',
-    type=click.Path(exists=True, file_okay=False, dir_okay=True)
+    "--video_dir",
+    prompt="Directory where videos are located",
+    default="./data/raw",
+    type=click.Path(exists=True, file_okay=False, dir_okay=True),
 )
 @click.option(
-    '--cache_dir',
-    prompt='Directory where feature extraction cache is located',
-    default='./data/cache',
-    type=click.Path(exists=False, file_okay=False, dir_okay=True)
+    "--cache_dir",
+    prompt="Directory where feature extraction cache is located",
+    default="./data/cache",
+    type=click.Path(exists=False, file_okay=False, dir_okay=True),
 )
 @click.option(
-    '--output_file',
-    prompt='Directory where feature extraction cache is located',
-    default='./model.pkl',
-    type=click.Path(exists=False, file_okay=True, dir_okay=False)
+    "--output_file",
+    prompt="Directory where feature extraction cache is located",
+    default="./model.pkl",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False),
 )
 @click.option(
-    '--algorithm',
-    prompt='Algorithm to use',
-    type=click.Choice(list(algorithmFunctions.keys())),
-    cls=ChoiceOption
-)
-@click.option(
-    '--feature_amount',
+    "--feature_amount",
     default=320,
-    prompt='Feature amount to select',
+    prompt="Feature amount to select",
     type=click.IntRange(min=5),
 )
+@click.option(
+    "--algorithm",
+    prompt="Algorithm to use",
+    type=click.Choice(list(algorithmFunctions.keys())),
+    cls=ChoiceOption,
+)
 def main(video_dir, cache_dir, output_file, algorithm, feature_amount):
-    clf = algorithmFunctions[algorithm]()
+    global clf
+    algorithmFunctions[algorithm](standalone_mode=False)
 
-    save_stderr = sys.stderr
-    sys.stderr = io.BytesIO()
+    with suppress_stdout_stderr():
+        misc_df, classic_df, fresh_df, y = get_data(Path(video_dir), Path(cache_dir))
+        data = pd.concat([misc_df, classic_df, fresh_df], axis=1)
 
-    misc_df, classic_df, fresh_df, y = get_data(Path(video_dir), Path(cache_dir))
-    data = pd.concat([misc_df, classic_df, fresh_df], axis=1)
+        pipe = Pipeline(
+            [
+                (
+                    "scaler",
+                    QuantileTransformer(n_quantiles=50).set_output(transform="pandas"),
+                ),
+                ("select", FeatureSelector(n_features=feature_amount)),
+                ("model", clf),
+            ]
+        )
 
-    pipe = Pipeline([
-        ("scaler", QuantileTransformer(n_quantiles=50).set_output(transform="pandas")),
-        ("select", FeatureSelector(n_features=feature_amount)),
-        ("model", clf),
-    ])
-
-    sys.stderr = save_stderr
-
-    pipe.fit(data, y)
+        pipe.fit(data, y)
 
     with open(Path(output_file), "wb") as f:
         pickle.dump(pipe, f)
 
-    click.echo(f"Trained model saved at {output_file}. You can go to https://paddel.catalin.sh/ and upload it.")
+    click.echo(
+        f"Trained model saved at {output_file}. You can go to https://paddel.catalin.sh/ and upload it."
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
