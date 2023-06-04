22078089ccb7786a1e170062cafdd5af610b7c60
cataand <52856673+cataand@users.noreply.github.com>
2023-06-04 11:03:12 +0200

    Add possibility to not use legends to plotting

--- paddel/src/paddel/plots.py
+++ paddel/src/paddel/plots.py
@@ -7,14 +7,14 @@ def select_by_values(data, column, *values):
 
 
 def plot_grid_search(
-    data: pd.DataFrame,
-    title: str,
-    param_left: str,
-    param_bottom: str,
-    param_legend: str,
-    param_left_name: str,
-    param_bottom_name: str,
-    param_legend_name: str,
+        data: pd.DataFrame,
+        title: str,
+        param_left: str,
+        param_bottom: str,
+        param_left_name: str,
+        param_bottom_name: str,
+        param_legend: str = None,
+        param_legend_name: str = None,
 ):
     """Generates and shows the results of a grid search done with sklean.
 
@@ -23,25 +23,35 @@ def plot_grid_search(
         title (str): Plot title.
         param_left (str): Parameter from data for the left axis.
         param_bottom (str): Parameter from data for the bottom axis.
-        param_legend (str): Parame from data for the legend.
+        param_legend (str): Parameter from data for the legend.
         param_left_name (str): Tag for the left axis.
         param_bottom_name (str): Tag for the bottom axis.
         param_legend_name (str): Tag for the legend.
     """
-    data = data.groupby([param_bottom, param_legend], as_index=False).agg({param_left: 'max'})
-
-    legends = data[param_legend].unique()
-
     _, ax = plt.subplots(1, 1)
 
-    for legend in legends:
-        legend_data = data[data[param_legend] == legend]
+    if param_legend and param_legend_name:
+        data = data.groupby([param_bottom, param_legend], as_index=False).agg({param_left: 'max'})
+        legends = data[param_legend].unique()
 
+        for legend in legends:
+            legend_data = data[data[param_legend] == legend]
+
+            ax.plot(
+                legend_data[param_bottom],
+                legend_data[param_left],
+                "-o",
+                label=f"{param_legend_name}: {legend}",
+            )
+
+        ax.legend(loc="best", fontsize=7)
+
+    else:
+        data = data.groupby([param_bottom], as_index=False).agg({param_left: 'max'})
         ax.plot(
-            legend_data[param_bottom],
-            legend_data[param_left],
+            data[param_bottom],
+            data[param_left],
             "-o",
-            label=f"{param_legend_name}: {legend}",
         )
 
     ax.set_title(title, fontsize=16, fontweight="bold")
@@ -50,5 +60,4 @@ def plot_grid_search(
         [item.get_text() for item in ax.get_xticklabels()], rotation=45, ha="right"
     )
     ax.set_ylabel(param_left_name, fontsize=12)
-    ax.legend(loc="best", fontsize=7)
     ax.grid("on")
